1db7c21eea6336022c4dfbd5931e5fa4c7b47914
Juan Villa <ing.villa.jimenez@gmail.com>
2023-12-11 15:39:17 -0600

    flat and flatMap

--- script.js
+++ script.js
@@ -599,7 +599,7 @@ const findAccount = function (accounts) {
 };
 const foundAccount = findAccount(accounts);
 console.log(foundAccount);
-*/
+
 
 ///////////////////////////////////////
 // some and every
@@ -624,3 +624,26 @@ const deposit = (mov) => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+*/
+
+///////////////////////////////////////
+// flat and flatMap
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// flat
+const overallBalance = accounts
+  .map((acc) => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
+
+// flatMap
+const overallBalance2 = accounts
+  .flatMap((acc) => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance2);
